163f9582b78488941e4776e8a0ae794d6efb780c
Bryandlc654 <bdelacruz654@gmail.com>
2023-05-25 20:03:53 -0500

    Actualizacion

--- Semana6/Quieres_ser_mi_novia/js/index.js
+++ Semana6/Quieres_ser_mi_novia/js/index.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
-const documentReady = () =>{
-  const heroTitle = document.getElementById('heroTitle');
-  const yesButton = document.getElementById('yesButton');
-  const noButton = document.getElementById('noButton');
+const documentReady = () => {
+  const heroTitle = document.getElementById("heroTitle");
+  const yesButton = document.getElementById("yesButton");
+  const noButton = document.getElementById("noButton");
 
-  const getPartner = () =>{
-    alert('❤️❤️❤️ Acepto ❤️❤️😍');
-    location.href ='https://www.youtube.com/watch?v=red8c8UXBlg&list=RD8ov2oFVXygM&index=2&ab_channel=CarlosZamora';
+  const getPartner = () => {
+    alert("❤️❤️❤️ Acepto ❤️❤️😍");
+    location.href = "https://www.youtube.com/watch?v=ZDgyVKHuG8M";
   };
-  const brokenHeart = () =>{
-    noButton.style.top = Math.random() * innerHeight + 'px';
-    noButton.style.left = Math.random() * innerWidth + 'px';
+  const brokenHeart = () => {
+    noButton.style.top = Math.random() * innerHeight + "px";
+    noButton.style.left = Math.random() * innerWidth + "px";
   };
-  const partner = prompt('😍 dime tu nombre 😍');
-  heroTitle.innerText += ` ${partner || 'Anonimo'} ?❤️`;
+  const partner = prompt("😍 dime tu nombre 😍");
+  heroTitle.innerText += ` ${partner || "Anonimo"} ?❤️`;
 
-  yesButton.addEventListener('click', getPartner);
-  noButton.addEventListener('mouseover',brokenHeart);
+  yesButton.addEventListener("click", getPartner);
+  noButton.addEventListener("mouseover", brokenHeart);
 };
 
-document.addEventListener('DOMContentLoaded',documentReady);
+document.addEventListener("DOMContentLoaded", documentReady);
---
+++ Semana9/1-patita-feliz-r/src/components/secions/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Patita Feliz 2000</h1>
+      <p>comida</p>
+    </header>
+  );
+};
+
+export default Header;
